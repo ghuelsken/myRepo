c4eeb9a883f5da7c87eb63dc973db159786a4002
Hülsken Gregor <ghuelsken@hotmail.de>
2019-12-05 16:26:36 +0100

    Projektdateien hinzufügen.

---
+++ HelloWorld1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld1
+{
+    class Program
+    {
+
+        /*
+         * Kommentare können mehrzeilig sein
+         */
+
+        static void Main(string[] args)
+        {
+            //Zeilenkommentar
+            String myName = "Gregor";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            Console.WriteLine("Es wurde der Name \" " + myName + "\" hinterlegt");
+            Console.ReadKey();
+        }
+    }
+}
